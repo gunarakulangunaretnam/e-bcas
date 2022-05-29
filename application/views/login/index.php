39282a888abe46e0b8c87f1b545fcc2c8a427f5c
GunaRakulan-GR <gunarakulan@gmail.com>
2022-05-29 17:03:14 +0530

    Remove forget password

--- application/views/login/index.php
+++ application/views/login/index.php
@@ -86,20 +86,6 @@
 						<span class="focus-input100"></span>
 					</div>
 					
-					<div class="flex-sb-m w-full p-b-48">
-						<div class="contact100-form-checkbox">
-							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-							<label class="label-checkbox100" for="ckb1">
-								Remember me
-							</label>
-						</div>
-
-						<div>
-							<a href="#" class="txt3">
-								Forgot Password?
-							</a>
-						</div>
-					</div>
 
 					<div class="container-login100-form-btn">
 						<button id="subBTN" name="sub" class="login100-form-btn">
